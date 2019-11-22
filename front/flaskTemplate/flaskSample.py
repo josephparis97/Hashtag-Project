8ff2816cf3028d989bd475266721ca4eb91f5ffd
Grégoire de Massé <demasse.gregoire@yahoo.fr>
2019-11-22 17:42:23 +0100

    pas localhost

--- front/flaskTemplate/flaskSample.py
+++ front/flaskTemplate/flaskSample.py
@@ -10,6 +10,6 @@ def home():
     return render_template('index.html')
 
 app.run(
-    host=app.config.get("HOST", "localhost"),
+    host=app.config.get("HOST", "0.0.0.0"),
     port=app.config.get("PORT", 5000)
 )
