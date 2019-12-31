80b20269440f9beeb4e71d31ec227e9c99056ec0
i_am_natha <r.nathanael@hotmail.com>
2019-12-31 11:45:49 +0100

    Commit

--- front/flaskTemplate/flaskSample.py
+++ front/flaskTemplate/flaskSample.py
@@ -1,9 +1,11 @@
 import flask
 from flask import render_template
 from flask import request
+from flask_cors import CORS
 import requests
 
 app = flask.Flask(__name__)
+CORS(app, send_wildcard=True)
 app.config['DEBUG'] = True
 
 ALLOWED_EXTENSIONS = set(['jpg', 'png'])
@@ -51,7 +53,19 @@ def image():
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    return response
+
+if __name__ == '__main__':
+    import os
+    port = os.environ.get("port", 5000)
+    app.run(debug=True, port=port, host="0.0.0.0")
+'''
 app.run(
     host=app.config.get("HOST", "0.0.0.0"),
     port=app.config.get("PORT", 5000)
 )
+'''
